8bd08c504bbfebbd9aa2b190797020e1abff212e
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-14 21:15:55 -0500

    added delete route handler

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -2,6 +2,7 @@ const fsPro = require('fs').promises;
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const {v4: uuid} = require('uuid');
 
 router.get('/', async (req, res) => {
   const data = await fsPro.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8');
@@ -14,7 +15,8 @@ router.post('/', async (req, res) => {
     const array = JSON.parse(data);
     const newObj = {
       title: req.body.title,
-      text: req.body.text
+      text: req.body.text,
+      id: uuid()
     }
     array.push(newObj);
     await fsPro.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(array));
@@ -27,6 +29,36 @@ router.post('/', async (req, res) => {
       message: 'bad request!'
     })
   }
+});
+
+router.delete('/:id', async (req, res) => {
+  let idExist = false;
+  const idToDelete = req.params.id;
+  console.log(idToDelete);
+  const data = await fsPro.readFile(path.join(__dirname, '..', 'db', 'db.json'), 'utf8');
+  const array = JSON.parse(data);
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id === idToDelete) {
+      idExist = true;
+    }
+  }
+
+  if (idExist) {
+    const newArray = array.filter((el) => el.id !== idToDelete);
+    console.log(newArray);
+    await fsPro.writeFile(path.join(__dirname, '..', 'db', 'db.json'), JSON.stringify(newArray));
+    res.json({
+      status: "success",
+      message: "item deleted!"
+    })
+  } else {
+    res.status(400).json({
+      status:"fail",
+      message: "bad request: id not found"
+    })
+  }
+  
 });
 
 module.exports = router;
